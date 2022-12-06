24a82edeaae9ee6f988ce66653b185f7fbdc774c
Chandan2812 <chandanpms@gmail.com>
2022-12-06 20:59:03 +0530

    check prime

---
+++ prime.js
@@ -0,0 +1,15 @@
+function isprime(n){
+  let count=0;
+  for(let i=1;i<=n;i++){
+    if(n%i==0){
+      
+    }
+  }
+  if(count==2){
+    console.log(n,"is prime");
+  }
+  else{
+    console.log(n,"is not prime");
+  }
+}
+isprime(5)
